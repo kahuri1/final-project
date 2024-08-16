8a0ee7e3094e103be27d7eae0f5462494fee9546
kahuri1 <164179905+kahuri1@users.noreply.github.com>
2024-08-16 20:29:07 +0300

    Add files via upload
    тестовый

---
+++ tests/settings.go
@@ -0,0 +1,7 @@
+package tests
+
+var Port = 7540
+var DBFile = "../scheduler.db"
+var FullNextDate = true
+var Search = false
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZF9oYXNoIjoiOTg3NjAiLCJleHAiOjE3MjM4MDcwNjN9.Bibz0x4wCIiaXls5k_ndutPB0JPAKWOKqhpipPKtsFM`
